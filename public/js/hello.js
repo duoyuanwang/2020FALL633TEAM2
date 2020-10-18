170375c6307c9e89558fc92f4ca71713b21da8d1
Duoyuan Wang <duoyuan@aerospec.us>
2020-10-17 21:01:03 -0400

    initial commit.

---
+++ public/js/hello.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCyL8SqsTvp7fiJo0lqRymTQsfOlNS4d4o",
+    authDomain: "lfa-grouping-t2.firebaseapp.com",
+    databaseURL: "https://lfa-grouping-t2.firebaseio.com",
+    projectId: "lfa-grouping-t2",
+    storageBucket: "lfa-grouping-t2.appspot.com",
+    messagingSenderId: "506414467497",
+    appId: "1:506414467497:web:b62a6724962136da581cb8",
+    measurementId: "G-RGJ21MRTWB"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();

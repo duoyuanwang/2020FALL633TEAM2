6ba6326b50724f6a06ad5792aeffb74f63eb2e23
vinMoo <mokun.vin@gmail.com>
2020-11-08 17:24:19 -0500

    The frames of instructor and student page

---
+++ public/js/instructor.js
@@ -0,0 +1,13 @@
+/* 
+window.addEventListener("DOMContentLoaded", event => {
+    document.getElementById("addlist").addEventListener("click", event => {
+        addnewlist();
+   });
+   
+});
+
+function addnewlist() {
+    text = '<input type="text" class="newlist">'+'<br>';
+    document.getElementById("list").innerHTML = text;
+}
+*/

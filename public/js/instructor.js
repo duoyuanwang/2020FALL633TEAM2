5fcce68028e3ecffd853a64629252f3f13404a39
Duoyuan Wang <duoyuan@aerospec.us>
2020-11-14 21:42:11 -0500

    All static scketchs

--- public/js/instructor.js
+++ public/js/instructor.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     console.log("ready");
-    $("#survey-detail").attr("src", "instructor_detail.html")
+    $("#survey-detail").attr("src", "instructor_result.html")
 });
---
+++ public/js/student.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    console.log("ready");
+    $("#detail").attr("src", "student_detail.html")
+});
